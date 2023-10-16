48888227dcf4911f255d625238dc8e71f27ea820
Vicktor007 <vicktord007@gmail.com>
2023-10-16 20:30:53 +0100

    save

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import { PageWrapper } from "../components/styles/PageWrapper"
-import LoginForm from '../components/utilities/LoginForm'
-import Title from '../components/utilities/Title'
-import Footer from '../components/pages/Footer'
+import React, { useContext } from "react";
 
+// context
+
+import { AuthenticationContext } from "../context/AuthenticationContext";
+import LoginForm from "../components/utilities/LoginForm";
+import Title from "../components/utilities/Title";
+import { PageWrapper } from "../components/styles/PageWrapper";
+import Footer from "../components/pages/Footer";
+import Button from "../components/utilities/Button";
 export default function Home() {
+  const authenticator = useContext(AuthenticationContext);
   return (
-    <PageWrapper className='container'>
-      <Title text='Welcome'/>
-     <LoginForm/>
-     <Footer/>
+    <PageWrapper className="animate-left">
+      <Title text="Welcome" />
+      {!authenticator.isAuthenticated ? (
+        <LoginForm />
+      ) : (
+        <Button
+          onClick={authenticator.logout}
+          text="Logout"
+          classes={"btn-warning btn-block"}
+        />
+      )}
+      <Footer />
     </PageWrapper>
-  )
+  );
 }
