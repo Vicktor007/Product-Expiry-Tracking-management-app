23af448c8a160138e4f6d69b7c81ca6f51d3552f
Vicktor007 <vicktord007@gmail.com>
2023-10-20 16:50:40 +0100

    save

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <div className='container animate-left text-center f-column'>
       <Title text='Welcome to Your'/>
-      <h3 className="subtitle">Product Expiry Date tracker</h3>
+      <h3 className="subtitle">Product Expiry Date Tracking System</h3>
     </div>
   )
 }
