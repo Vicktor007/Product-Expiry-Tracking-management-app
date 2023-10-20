cfa32df2b9071a7ac1c79b0267ee3cc12b0546e3
Vicktor007 <vicktord007@gmail.com>
2023-10-20 16:52:14 +0100

    save

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ import Title from '../components/utilities/Title'
 export default function Index() {
   return (
     <div className='container animate-left text-center f-column'>
-      <Title text='Welcome to Your'/>
+      <Title text='Product Expiry Date Tracking System'/>
       <h3 className="subtitle">Product Expiry Date Tracking System</h3>
     </div>
   )
